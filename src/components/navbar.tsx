88a64b3dc6db54e13c09b6bb9a2e7047e6877247
JayCodeGitHub <JayCode.contakt@gmail.com>
2023-02-07 15:58:14 +0100

    Styled NavBar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,13 +2,15 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <ul>
-      <li>
-        <Link href="/">Home</Link>
-      </li>
-      <li>
-        <Link href="/contact">Contact</Link>
-      </li>
-    </ul>
+    <nav className="text-1xl p-4">
+      <ul className="flex gap-12">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
